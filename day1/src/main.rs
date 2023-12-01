4eb1c99a4e595b20dbeb33de89a43017fb45449b
Jasper de Vries <jasper.devries@the-future-group.com>
2023-12-01 21:58:54 +0100

    Day 1 remove nom

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,11 +1,4 @@
 use anyhow::Result;
-use nom::{
-    character::complete::{alpha0, line_ending, one_of},
-    combinator::map_res,
-    multi::{many1, separated_list1},
-    sequence::delimited,
-    IResult,
-};
 
 const DATA: &str = include_str!("input.txt");
 
@@ -37,39 +30,28 @@ fn part_two(input: &[u32]) -> u32 {
     input.iter().sum()
 }
 
-fn parse_one(input: &str) -> IResult<&str, Vec<u32>> {
-    separated_list1(line_ending, parse_line)(input)
-}
-
-fn parse_line(input: &str) -> IResult<&str, u32> {
-    map_res(parse_digits, |v| {
-        let a = v.first().unwrap();
-        let b = v.last().unwrap();
-        format!("{a}{b}").parse::<u32>()
-    })(input)
-}
-
-fn parse_digits(input: &str) -> IResult<&str, Vec<char>> {
-    many1(delimited(alpha0, parse_single_digit, alpha0))(input)
-}
-
-fn parse_single_digit(input: &str) -> IResult<&str, char> {
-    one_of("1234567890")(input)
-}
-
 fn parse_input_one(input: &'static str) -> Result<Vec<u32>> {
-    let (_, input) = parse_one(input)?;
+    let input = parse(input, false);
 
     Ok(input)
 }
 
-fn simple_parse(input: &str) -> Vec<u32> {
-    input.lines().map(simple_parse_line).collect()
+fn parse_input_two(input: &'static str) -> Result<Vec<u32>> {
+    let input = parse(input, true);
+
+    Ok(input)
 }
 
-fn simple_parse_line(input: &str) -> u32 {
-    let digits = (0..input.len())
-        .filter_map(|i| match_to_char(&input[i..input.len()]))
+fn parse(input: &str, use_words: bool) -> Vec<u32> {
+    input
+        .lines()
+        .map(|line| parse_line(line, use_words))
+        .collect()
+}
+
+fn parse_line(line: &str, use_words: bool) -> u32 {
+    let digits = (0..line.len())
+        .filter_map(|i| match_to_char(&line[i..], use_words))
         .collect::<Vec<char>>();
 
     format!("{}{}", digits.first().unwrap(), digits.last().unwrap())
@@ -77,45 +59,27 @@ fn simple_parse_line(input: &str) -> u32 {
         .unwrap()
 }
 
-fn match_to_char(input: &str) -> Option<char> {
-    if input.starts_with('1') || input.starts_with("one") {
-        return Some('1');
+fn match_to_char(input: &str, use_words: bool) -> Option<char> {
+    if let Some(first_char) = input.chars().next() {
+        if first_char.is_ascii_digit() {
+            return Some(first_char);
+        }
     }
-    if input.starts_with('2') || input.starts_with("two") {
-        return Some('2');
-    }
-    if input.starts_with('3') || input.starts_with("three") {
-        return Some('3');
-    }
-    if input.starts_with('4') || input.starts_with("four") {
-        return Some('4');
-    }
-    if input.starts_with('5') || input.starts_with("five") {
-        return Some('5');
-    }
-    if input.starts_with('6') || input.starts_with("six") {
-        return Some('6');
-    }
-    if input.starts_with('7') || input.starts_with("seven") {
-        return Some('7');
-    }
-    if input.starts_with('8') || input.starts_with("eight") {
-        return Some('8');
-    }
-    if input.starts_with('9') || input.starts_with("nine") {
-        return Some('9');
-    }
-    if input.starts_with('0') {
-        return Some('0');
+
+    if use_words {
+        if let Some(c) = [
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ]
+        .iter()
+        .enumerate()
+        .find(|(_, word)| input.starts_with(*word))
+        .and_then(|(i, _)| char::from_digit(i as u32, 10))
+        {
+            return Some(c);
+        }
     }
 
     None
-}
-
-fn parse_input_two(input: &'static str) -> Result<Vec<u32>> {
-    let input = simple_parse(input);
-
-    Ok(input)
 }
 
 #[cfg(test)]
